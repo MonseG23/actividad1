dd26b89d74d7d92f1cf31df0a593686a72f32951
MonseG23 <monserrat2003.23@gmail.com>
2025-02-28 22:55:37 -0600

    actividad

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\universal;
 
+
 Route::get('/', function () {
     echo 'hello guys! this is y first laravel';
     echo '<pre>';
     print_r (universe ::all());
     echo '<pre>';
     
-    dump (universe ::all());
-
-
-    
+    //dump (universe ::all());
+    //SELECT FROM universes
     //return view('welcome');
 });
